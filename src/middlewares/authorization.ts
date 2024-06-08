d4842c1ceeaf15dc6734c039ef115bca91a12440
Hakan Kaygusuz <hakankaygusuzone@gmail.com>
2024-06-08 22:38:11 +0300

    x

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -4,14 +4,26 @@ import { getUser } from "../models/user";
 
 export = async(req: Request, res: Response, next: NextFunction) => {
     const { token } = req.cookies;
-    if (!token) return res.status(500).json({ message: "Invalid token"});
 
-    const UserToken = await Jwt.verifyToken(token);
-    if (!UserToken) return res.status(500).json({ message: "token cannot be used" });
-
-    const user = await getUser({ _id: UserToken.id }); 
-    if (!user) return res.status(500).json({ message: "user not found" });
-
-    req.user = user;
-    next();
+    if (!token) {
+        return res.status(400).json({ message: "Token is missing" });
+    }
+    
+    try {
+        const decodedToken = await Jwt.verifyToken(token);
+        if (!decodedToken) {
+            return res.status(401).json({ message: "Invalid token" });
+        }
+    
+        const user = await getUser({ _id: decodedToken.id });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error("Token verification or user retrieval failed", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
 }
